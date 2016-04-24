9e13efc86d2ca1c12f667bac42f1112e8067a606
d3viant0ne <joshuaw@easymetrics.com>
2016-04-24 14:42:07 -0500

    Add material elements to home component

--- src/client/app/+home/components/home.component.ts
+++ src/client/app/+home/components/home.component.ts
@@ -1,5 +1,9 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
+import {MdButton} from '@angular2-material/button';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 import {NameListService} from '../../shared/index';
 
@@ -7,7 +11,7 @@ import {NameListService} from '../../shared/index';
   selector: 'sd-home',
   templateUrl: 'app/+home/components/home.component.html',
   styleUrls: ['app/+home/components/home.component.css'],
-  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, MdButton, MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES, MD_LIST_DIRECTIVES]
 })
 export class HomeComponent {
   newName: string;
